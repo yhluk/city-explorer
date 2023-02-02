c168b89548c3faede286e0904235aa4025ce336a
yurii hlukhyi <yhlukhyi@gmail.com>
2023-02-01 17:12:20 -0800

    some styling

--- src/Weather.js
+++ src/Weather.js
@@ -6,29 +6,23 @@ import { Card, Container } from "react-bootstrap";
 
 class Weather extends React.Component {
   render() {
-    // console.log("weather in card", this.props.weatherDay[0].description)
     return (
-   
-      this.props.weatherDay.map((day, idx) =>
-        <Container  >
-         
-              <Card
-              border="success"
-                style={{
-                  width: '10rem',
-                  height: 'auto',
-                  display: 'flex',
-                  border: '2px solid'
-                }}
-                key={idx}>
-                <Card.Title>{day.description}</Card.Title>
-                <Card.Text>{day.date}</Card.Text>
-                <Card.Img
-                src="https://i.pinimg.com/564x/a4/e1/ce/a4e1ce66480e82f91395b3560653a827.jpg" />
-              </Card>
-          
+      <Container  >
+      {this.props.weatherDay.map((day, idx) =>
+          <Card
+            border="success"
+            style={{
+              width: '10rem'
+            }}
+            key={idx}>
+            <Card.Title>{day.description}</Card.Title>
+            <Card.Text>{day.date}</Card.Text>
+            <Card.Img
+              src="https://i.pinimg.com/564x/a4/e1/ce/a4e1ce66480e82f91395b3560653a827.jpg" />
+          </Card>
+
+      )}
         </Container>
-      )
     )
   }
 }
