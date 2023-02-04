81991e0290a03562a7394c2376ebde58ac765702
yurii hlukhyi <yhlukhyi@gmail.com>
2023-02-03 23:07:59 -0800

    final changes to lab8

---
+++ src/Moviemodal.js
@@ -0,0 +1,35 @@
+
+
+// import React from "react"
+// import Modal from "react-bootstrap/Modal"
+// import './index.css'
+
+
+// class MovieModal extends React.Component {
+//   render() {
+//     return (
+//       <>
+//         <Modal
+//         className ={"modal"}
+//           show={this.props.showModal}
+//           onHide={this.props.handleCloseModal}>
+
+//           <Modal.Header closeButton>
+//             <Modal.Title>{this.props.title}</Modal.Title>
+//           </Modal.Header>
+
+//           <Modal.Body>
+//             <img src={this.props.image_url} alt={this.props.description}/>
+//           </Modal.Body>
+
+//           <Modal.Footer>
+//             {this.props.description}
+//           </Modal.Footer>
+          
+//         </Modal>
+
+//       </>
+//     );
+//   }
+// }
+// export default MovieModal
